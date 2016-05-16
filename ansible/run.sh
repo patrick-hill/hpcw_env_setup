d15ae02e2b201e27242a382b83bae05c17672fb2
Patrick Hill <Hill@HillsPCWorld.com>
2016-05-15 22:51:18 -0400

    EH Playbook & Refactoring
    * New playbook for EventHorizon
    * Moved playbook vars to role defaults
    * Dropbox daemon launched by ansible now
    * NFS tasks broken out to seperate role
    * Troubleshooting vagrant plugin installation

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,1 +1,8 @@
-ansible-playbook -i hosts playbook-devhouse.yml --user=vagrant --ask-sudo-pass
+#!/usr/bin/env bash
+
+if [ $# -gt 0 ]; then
+	ansible-playbook -i hosts $1 --ask-sudo-pass
+else
+	ansible-playbook -i hosts playbook-devhouse.yml --user=vagrant --ask-sudo-pass
+fi
+
